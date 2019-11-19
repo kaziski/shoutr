3411152447e72bc4faa1c1f0e4eb193586bcbe86
Kazumi Karbowski <kazkazinus@gmail.com>
2019-11-18 23:16:12 -0500

    Redirects to dashboard when signed in

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root to: "homes#show"
+  constraints Clearance::Constraints::SignedIn.new do
+    root to: "dashboards#show"
+  end
   resources :passwords, controller: "clearance/passwords", only: [:create, :new]
   resource :session, only: [:create]
 
