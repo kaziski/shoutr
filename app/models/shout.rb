b5ce3d38484ed7ecc69387c9b1a4ef8113f96ead
Kazumi Karbowski <kazkazinus@gmail.com>
2019-11-19 07:50:40 -0500

    Added polymorphic migration

--- app/models/shout.rb
+++ app/models/shout.rb
@@ -1,5 +1,6 @@
 class Shout < ApplicationRecord
   belongs_to :user
+  belongs_to :content, polymorphic: true
 
   validates :body, presence: true, length: { in: 1 ..144}
   validates :user, presence: true
